067d407bf46f671f55f58b857addf24e448102ba
limabia <bianca.lima.1504@gmail.com>
2017-12-11 00:04:42 -0200

    continuacao da leitura de arquivo

--- ep/EP.java
+++ ep/EP.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
  
 public class EP {
  
     public static void main(String[] args) {
         if (args.length == 2){
+            //  dados fornecidos pelo usuario: nome do arquivo com o mapa e qual criterio de percurso sera usado
             String nome_arquivo = args[0];
             int criterio = Integer.parseInt(args[1]); 
-            
+            // tentativa para leitura do arquivo
             try {
                 FileReader arq = new FileReader(nome_arquivo);
                 BufferedReader lerArq = new BufferedReader(arq);
@@ -35,30 +36,32 @@ public class EP {
                 }
 
                 int qntItens = Integer.parseInt(lerArq.readLine());
-                String[][] conjuntoDeItens = new String[qntItens] [4];
-                System.out.println(qntItens);
+                int[][] conjuntoDeItens = new int[qntItens] [4];
+                
                 for (int x=0; x < qntItens; x++){
                     String linhaItem = lerArq.readLine();
-                    
-                    conjuntoDeItens[x][0] = linhaItem.substring(0,1);
-                    conjuntoDeItens[x][1] = linhaItem.substring(1,2);
-                    conjuntoDeItens[x][2] = linhaItem.substring(2,3);
-                    conjuntoDeItens[x][3] = linhaItem.substring(3,4);
-
-                    System.out.printf("\n%s", conjuntoDeItens[x][0]);
-                    System.out.printf("%s", conjuntoDeItens[x][1]);
-                    System.out.printf("%s", conjuntoDeItens[x][2]);
-                    System.out.printf("%s", conjuntoDeItens[x][3]);
-
+                    //System.out.printf(linhaItem);
+                    String[] itens = linhaItem.split(" ");
+                    conjuntoDeItens[x] [0] =  Integer.parseInt(itens[0]); // linha
+                    conjuntoDeItens[x] [1] =  Integer.parseInt(itens[1]); // coluna
+                    conjuntoDeItens[x] [2] =  Integer.parseInt(itens[2]); // valor
+                    conjuntoDeItens[x] [3] =  Integer.parseInt(itens[3]); // peso
                 }
+
+                // capacidade da mochila
+                int capacidadeMochila = Integer.parseInt(lerArq.readLine());
+
+                // coordenadas de partida
+
+                // coordenadas de chegada
                 arq.close();
             } 
+            // exception para casos de erro na abertura do arquivo
             catch (IOException e) {
                 System.err.printf("Erro na abertura do arquivo, tente novamente. \n Erro encontrado: %s.\n", e.getMessage());
             }
-            System.out.println();
         }
-
+        // mensagem para quando o usuario nao informar os 2 parametros necessarios
         else {
             System.err.println("eh preciso adicionar os parâmetros nome do arquivo e criterio ao rodar o programa.");
         } 
